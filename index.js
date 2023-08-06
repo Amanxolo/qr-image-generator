2151bc54ab29813635372f7d72a100f279df606d
Aman Tyagi <130159008+Amanxolo@users.noreply.github.com>
2023-08-06 15:51:48 +0530

    files added

---
+++ index.js
@@ -0,0 +1,24 @@
+import inquirer from 'inquirer'; //To use import instead of require we have to add type module in package.json
+import qr from 'qr-image';
+import fs from "fs";
+
+inquirer  //see documentation
+  .prompt([{
+    message: "Type in your URL: ",
+    name: "URL"}])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_image.png'));
+    fs.writeFile('url.txt', url, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      }); 
+    })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
